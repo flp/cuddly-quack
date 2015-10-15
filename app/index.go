6be1ec462896220764ae182030394cb81218687f
Kelly Dunn <kellydunn@simple.com>
2015-10-15 12:23:54 -0700

    app: Adding in app package

---
+++ app/index.go
@@ -0,0 +1,11 @@
+package app
+
+import (
+	"net/http"
+)
+
+type IndexHandler struct {}
+
+func (i *IndexHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("hello world"))	
+}
