9c486fa60dde0f98ea67c5494c6ad6912eb76221
Franklin Pearsall <franklin.pearsall@gmail.com>
2015-11-09 14:42:29 -0800

    Generate a random pack from mtgjson data for Battle for Zendikar.
    New file: mtg/json.go. json.go contains all code for reading json card
    data.
    
    To generate a pack, first seed the rng and read the json data stored in
    bindata.go. Then call GeneratePack() in mtg/packs.go.
    
    Currently cards only have the card's name and the card's rarity.

--- mtg/card.go
+++ mtg/card.go
@@ -1,15 +1,30 @@
 package mtg
 
+import (
+	"fmt"
+)
+
 type CardRarity int
 
-const(
-	Common		CardRarity = 0
-	Uncommon	CardRarity = 1
-	Rare		CardRarity = 2
-	Mythic		CardRarity = 3
+const (
+	Common    CardRarity = 0
+	Uncommon  CardRarity = 1
+	Rare      CardRarity = 2
+	Mythic    CardRarity = 3
+	Special   CardRarity = 4
+	BasicLand CardRarity = 5
 )
 
 type Card struct {
-	Rarity	CardRarity
-	Foil	bool
+	Name   string "name"
+	Rarity string "rarity"
+}
+
+type Set struct {
+	Name  string  "name"
+	Cards []*Card "cards"
+}
+
+func (c Card) String() string {
+	return fmt.Sprintf("NAME: %s, RARITY: %s", c.Name, c.Rarity)
 }
--- mtg/packs.go
+++ mtg/packs.go
@@ -1,9 +1,81 @@
 package mtg
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Pack struct {
 	Cards []*Card
 }
 
+func (p Pack) String() string {
+	str := ""
+	for _, card := range p.Cards {
+		str += fmt.Sprintf("%s\n", card)
+	}
+	return str
+}
+
 type PackGenerator interface {
 	GeneratePack() Pack
+}
+
+type ModernPackGenerator struct {
+}
+
+// ChooseNCards chooses n cards at random from an array of cards.
+func ChooseNCards(cards []*Card, n int) []*Card {
+	if n > len(cards) {
+		// TODO(flp): better error handling
+		return nil
+	}
+
+	shuffle := func(a []*Card) {
+		for i := range a {
+			j := rand.Intn(i + 1)
+			a[i], a[j] = a[j], a[i]
+		}
+	}
+
+	var result []*Card
+	var cardsCopy = make([]*Card, len(cards))
+	copy(cardsCopy, cards)
+	for i := 0; i < n; i++ {
+		shuffle(cardsCopy)
+		result = append(result, cardsCopy[0])
+		cardsCopy = cardsCopy[1:]
+	}
+
+	return result
+}
+
+// ModernPackGenerator's GeneratePack creates a pack with 14 cards: 1 random rare, 3 random
+// uncommons, and 10 random commons.
+func (mpg ModernPackGenerator) GeneratePack() Pack {
+	// TODO(flp): we should be able to distinguish between sets
+	// TODO(flp): foils
+	p := Pack{}
+
+	// Get rare (possibly a mythic rare)
+	n := rand.Intn(8)
+	if n == 7 {
+		// Choose a mythic rare instead of a normal rare
+		mythics := RaritiesToCards[Mythic]
+		p.Cards = append(p.Cards, ChooseNCards(mythics, 1)...)
+	} else {
+		// Choose a normal rare
+		rares := RaritiesToCards[Rare]
+		p.Cards = append(p.Cards, ChooseNCards(rares, 1)...)
+	}
+
+	// Get uncommons
+	uncommons := RaritiesToCards[Uncommon]
+	p.Cards = append(p.Cards, ChooseNCards(uncommons, 3)...)
+
+	// Get commons
+	commons := RaritiesToCards[Common]
+	p.Cards = append(p.Cards, ChooseNCards(commons, 10)...)
+
+	return p
 }
